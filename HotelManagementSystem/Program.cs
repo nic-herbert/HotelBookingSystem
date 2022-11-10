75114849a8c748b747f88ecac53e20213f8c3fc7
Nic Herbert <nicholasglherbert@gmail.com>
2022-11-09 19:56:21 -0500

    added email validation

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -2,9 +2,12 @@
 //using System.Runtime.CompilerServices;
 
 
-//Console.Clear();
+
 
 using HotelManagementSystem;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
 
 Console.WriteLine("Choose an option:");
 Console.WriteLine("1) Book reservation");
@@ -17,44 +20,127 @@ rooms.Add(new Room()
 {
     isReserved = false,
     roomType = RoomType.SingleBed,
-    roomNumber = "502",
-    floorNumber = 5
+    roomNumber = "123",
+    floorNumber = 1
 }); ; ;
 
 rooms.Add(new Room()
 {
     isReserved = false,
-    roomType = RoomType.SingleBed,
-    roomNumber = "503",
-    floorNumber = 5
+    roomType = RoomType.DoubleBed,
+    roomNumber = "205",
+    floorNumber = 2
 }); ;
 
 rooms.Add(new Room()
 {
     isReserved = false,
-    roomType = RoomType.SingleBed,
-    roomNumber = "504",
-    floorNumber = 5
+    roomType = RoomType.KingSuite,
+    roomNumber = "1729",
+    floorNumber = 10
+
+  
 }); ;
 
-Console.WriteLine("Enter 3 to exit");
-string line;
-while ((line = Console.ReadLine()) != "3")
+rooms.Add(new Room()
 {
-    string result = (line);
-    Console.WriteLine(result);
+    isReserved = true,
+    roomType = RoomType.SingleBed,
+    roomNumber = "319",
+    floorNumber = 3
 
-    if (line == "3") 
- {
+
+}); ;
+
+
+
+
+//Console.ReadLine();
+
+string index = Console.ReadLine();
+while (index != "")
+{
+
+    // book a reservation
+    if (index == "1") 
         
-    }
+    {
+        Console.WriteLine("Enter the room you'd like to book:");
+        var vacantRooms = rooms.Where(room => room.isReserved == false).ToList();
+        foreach (var room in vacantRooms)
+        {
+            Console.WriteLine(room.roomNumber);
+        }
 
-    if (line == "2")
+        string roomNumber = Console.ReadLine();
+        
+        var roomToBeReserved = vacantRooms.Find(room => room.roomNumber == roomNumber);
+        
+        Console.WriteLine("Please enter your email for confirmation.");
+        string guestEmail = Console.ReadLine();
+
+        Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        Match match = regex.Match(guestEmail);
+        if (match.Success)
+        {
+            roomToBeReserved.contactEmail = guestEmail;
+            Console.WriteLine("Thank you for booking a room! We'll send you a confirmation email at " + guestEmail);
+            roomToBeReserved.isReserved = true;
+        }
+        else
+        {
+            Console.WriteLine("Please enter valid email: ");
+        }
+        
+        DisplayMenu();
+
+
+   }
+
+    // add a room
+    if (index == "2")
+ 
     {
         var newRoom = new Room();
+        Console.WriteLine("Enter room number:");
         string roomName = Console.ReadLine();
+        Console.WriteLine("Enter bed type:");
+        var values = Enum.GetValues(typeof(RoomType));
+        foreach (var value in values)
+        {
+            Console.WriteLine(value);
+        }
+        
+        string bedType = Console.ReadLine();
+        Enum.TryParse(bedType, out RoomType roomType);
+        newRoom.roomType = roomType;
+        Console.WriteLine("Enter room floor");
+        string roomFloor = Console.ReadLine();
+        newRoom.floorNumber = Int32.Parse(roomFloor);
         newRoom.roomNumber = roomName;
         newRoom.isReserved = false;
         rooms.Add(newRoom);
+        Console.WriteLine("Thank you for adding a room.");
+
+        DisplayMenu();
     }
+
+    // exit
+    if (index == "3")
+    {
+        
+    }
+
+    index = Console.ReadLine(); 
+
+}
+
+void DisplayMenu()
+{
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1) Book reservation");
+    Console.WriteLine("2) Add room to system");
+    Console.WriteLine("3) Exit");
+    Console.Write("\r\nSelect an option: ");
+
 }
